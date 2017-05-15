b132ff6c59796d43403bb17ff0fa778b6bcca811
JerryLin <k2345777@gmail.com>
2017-05-15 19:14:34 +0800

    First Upload

---
+++ RS485test/SW4STM32/RS485test/Debug/Application/User/stm32f4xx_hal_msp.d
@@ -0,0 +1,80 @@
+Application/User/stm32f4xx_hal_msp.o: \
+ C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Src/stm32f4xx_hal_msp.c \
+ C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Inc/stm32f4xx_hal_conf.h \
+ C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Inc/main.h \
+ C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f405xx.h \
+ C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/CMSIS/Include/core_cm4.h \
+ C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/CMSIS/Include/core_cmSimd.h \
+ C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h
+
+C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Inc/stm32f4xx_hal_conf.h:
+
+C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Inc/main.h:
+
+C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f405xx.h:
+
+C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/CMSIS/Include/core_cm4.h:
+
+C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/CMSIS/Include/core_cmInstr.h:
+
+C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/CMSIS/Include/core_cmFunc.h:
+
+C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/CMSIS/Include/core_cmSimd.h:
+
+C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+C:/Users/Jerry/Desktop/HFV/STM32F405/RS485test/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
